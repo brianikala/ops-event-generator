08d5ced47dc27d228dc754c5ccfbdadba6b54434
Veck Hsiao <veck.hsiao@ikala.tv>
2021-12-18 02:48:50 +0800

    Add status check page (fake)

--- views/dashboard.py
+++ views/dashboard.py
@@ -9,4 +9,12 @@ dashboard = Blueprint('dashboard', __name__)
 
 @dashboard.route('/dashboard')
 def show():
+    er_status = check_er()
+    pubsub_status = check_pubsub()
     return render_template('dashboard.html')
+
+def check_pubsub():
+    return "up"
+
+def check_er():
+    return "down"
