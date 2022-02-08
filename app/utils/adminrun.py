30580db5dde89e7a3ed8f5dc7494cec4aebcf27e
JackywithaWhiteDog <b07705015@ntu.edu.tw>
2022-02-08 11:53:03 +0800

    feat: detail the logging message

--- app/utils/adminrun.py
+++ app/utils/adminrun.py
@@ -18,10 +18,21 @@ def get_base_url(customer):
 
 def get_enabled_events():
     """Get enabled events from Admin Run API"""
+    result = {}
     project_id = get_project_id()
+    if project_id is None:
+        print(json.dumps({
+            'severity': 'INFO',
+            'message': '[adminrun.py/get_enabled_events] Can not get Project ID. Please check the Application Default Credentials on Cloud Run.',
+        }))
+        return result
     customer = get_customer()
-    if project_id is None or customer is None:
-        return None
+    if customer is None:
+        print(json.dumps({
+            'severity': 'INFO',
+            'message': '[adminrun.py/get_enabled_events] Can not get Customer. Please check the environment variables on Cloud Run.',
+        }))
+        return result
     base_url = get_base_url(customer)
     # TODO: [API] Change to API /api/event_package?project_id=project_id to avoid passing customer name
     api_path = f'{base_url}/api/bq/get_enabled_events?customer={customer}&project_id={project_id}'
@@ -33,18 +44,17 @@ def get_enabled_events():
     }
     response = requests.get(api_path, headers=headers)
     message = ''
-    result = {}
     if response.status_code == 200:
         message = '[adminrun.py/get_enabled_events] Success'
         result = response.json()
         result['events'] = [event for event in result['events'] if event['enabled']]
         return result
     elif response.status_code == 404:
-        message = '[adminrun.py/get_enabled_events] The admin run for this customer does not exist'
+        message = '[adminrun.py/get_enabled_events] The admin run for this customer does not exist. Please make sure that the customer is registered on iKala Event Receiver Service.'
     elif response.status_code == 403:
-        message = '[adminrun.py/get_enabled_events] Service account is not added to admin run API'
+        message = '[adminrun.py/get_enabled_events] Service account is not added to admin run API. Please make sure that the Service Account used by the Cloud Run is added to the project on Admin Run Console.'
     else:
-        message = '[adminrun.py/get_enabled_events] No available enabled events of current customer'
+        message = '[adminrun.py/get_enabled_events] There are some internal server errors. Please make sure that the project is set properly on Admin Run Console, or contact the iKala.'
     print(json.dumps({
         'severity': 'INFO',
         'message': message,
